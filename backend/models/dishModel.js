a2c92198363a1fd7a30d0068195de958791f0f00
Staffilon <stanislav.teghipco@studenti.unicam.it>
2023-05-18 18:11:51 +0200

    Created the schema for the dish.

--- backend/models/dishModel.js
+++ backend/models/dishModel.js
@@ -1,1 +1,26 @@
 const mongoose = require("mongoose");
+
+const dishSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, "Please add the dish name"],
+    },
+    price: {
+        type: Number,
+        required: [true, "Please add the price of the dish"],
+    },
+    ingredients: [
+        {
+            name: {
+                type: String,
+                required: [true, "Please add the name of the ingredient"],
+            },
+            allergens: [
+                {
+                    type: String,
+                    required: [true, "Please add the name of the allergen"],
+                },
+            ],
+        },
+    ],
+});
