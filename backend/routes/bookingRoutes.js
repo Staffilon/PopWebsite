61592e3a40f8b30b7a713b4e6aa34576dd72dd0a
Staffilon <stanislav.teghipco@studenti.unicam.it>
2023-06-01 15:35:55 +0200

    Atomic commit.

--- backend/routes/bookingRoutes.js
+++ backend/routes/bookingRoutes.js
@@ -7,8 +7,14 @@ const {
     updateBooking,
     deleteBooking,
 } = require("../controllers/bookingController");
+const validateToken = require("../middleware/validateTokenHandler");
 
-router.route("/").get(getBookings).post(createBooking);
-router.route("/:id").get(getBooking).put(updateBooking).delete(deleteBooking);
+// Apply validateToken middleware to specific routes
+router.route("/").get(validateToken, getBookings).post(createBooking);
+router
+    .route("/:id")
+    .get(validateToken, getBooking)
+    .put(validateToken, updateBooking)
+    .delete(validateToken, deleteBooking);
 
 module.exports = router;
