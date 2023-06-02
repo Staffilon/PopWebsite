03328af2fb8b895cae708eaded1d69645c3822b3
Staffilon <stanislav.teghipco@studenti.unicam.it>
2023-06-02 19:53:42 +0200

    Removed unnecessary files.

--- frontend/PopCafe/react-files/src/pages/login.js
+++ frontend/PopCafe/react-files/src/pages/login.js
@@ -1,55 +1,57 @@
-import React, { useState } from 'react';
-import axios from 'axios';
+import axios from "axios";
+import React, { useState } from "react";
 
 const Login = () => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
 
-  const handleSubmit = async (event) => {
-    event.preventDefault();
+    const handleSubmit = async (event) => {
+        event.preventDefault();
 
-    try {
-      const response = await axios.post('/insert-user', {
-        username: email,
-        password: password,
-      });
+        try {
+            const response = await axios.post("/insert-user", {
+                username: email,
+                password: password,
+            });
 
-      console.log('User inserted:', response.data);
-    } catch (error) {
-      console.error('Error inserting user:', error);
-    }
-  };
+            console.log("User inserted:", response.data);
+        } catch (error) {
+            console.error("Error inserting user:", error);
+        }
+    };
 
-  return (
-    <div className="login-container">
-      <h1>Login</h1>
-      <form onSubmit={handleSubmit}>
-        <div className="form-group">
-          <label>Email address</label>
-          <input
-            type="email"
-            className="form-control"
-            placeholder="Enter email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            required
-          />
+    return (
+        <div className="login-container">
+            <h1>Login</h1>
+            <form onSubmit={handleSubmit}>
+                <div className="form-group">
+                    <label>Email address</label>
+                    <input
+                        type="email"
+                        className="form-control"
+                        placeholder="Enter email"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                        required
+                    />
+                </div>
+                <div className="form-group">
+                    <label>Password</label>
+                    <input
+                        type="password"
+                        className="form-control"
+                        placeholder="Password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                        required
+                    />
+                </div>
+                <button type="submit" className="btn btn-primary">
+                    Sign in
+                </button>
+            </form>
         </div>
-        <div className="form-group">
-          <label>Password</label>
-          <input
-            type="password"
-            className="form-control"
-            placeholder="Password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            required
-          />
-        </div>
-        <button type="submit" className="btn btn-primary">Sign in</button>
-      </form>
-    </div>
-  );
-}
+    );
+};
 
 export default Login;
