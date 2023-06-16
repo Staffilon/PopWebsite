70adcf31161cc04883df4d32172720ccd61f081f
Matteodife <matteodife1300@gmail.com>
2023-06-16 15:17:44 +0200

    Email notifications

--- backend/services/emailService.js
+++ backend/services/emailService.js
@@ -2,17 +2,20 @@ const nodemailer = require("nodemailer");
 
 // Configura il trasportatore di Nodemailer con le credenziali del tuo servizio di invio email
 const transporter = nodemailer.createTransport({
-    service: "Gmail",
+    host: 'smtp.gmail.com',
+    port: 587,
     auth: {
         user: "popcamerino@gmail.com",
         pass: "pzbbnyfovonenzvg", //2NXk#Cl6j5Fv
+    },
+    tls: {
+        rejectUnauthorized: false, // Accetta certificati SSL auto-firmati
     },
 });
 
 // Funzione per inviare la notifica via email
 const sendBookingNotification = async (bookingData) => {
-    const { type, date, time, numberOfPeople, name, surname, email } =
-        bookingData;
+    const { type, date, time, numberOfPeople, name, surname, email } = bookingData;
 
     // Crea il corpo del messaggio
     const message = `
@@ -24,10 +27,19 @@ const sendBookingNotification = async (bookingData) => {
     Nome: ${name}
     Cognome: ${surname}
     Email: ${email}
-  `;
+    `;
 
-    // Configura il messaggio email
-    const mailOptions = {
+    // Configura il messaggio email per l'utente che ha prenotato
+    const userMailOptions = {
+        from: "popcamerino@gmail.com",
+        to: email,
+        subject: "Conferma prenotazione",
+        text: message,
+        
+    };
+
+    // Configura il messaggio email per la notifica all'amministratore
+    const adminMailOptions = {
         from: "popcamerino@gmail.com",
         to: "popcamerino@gmail.com",
         subject: "Nuova prenotazione",
@@ -35,14 +47,15 @@ const sendBookingNotification = async (bookingData) => {
     };
 
     try {
-        // Invia l'email utilizzando il trasportatore di Nodemailer
-        await transporter.sendMail(mailOptions);
-        console.log("Notifica email inviata con successo!");
+        // Invia l'email di conferma all'utente che ha prenotato
+        await transporter.sendMail(userMailOptions);
+        console.log("Email di conferma inviata con successo all'utente!");
+
+        // Invia la notifica di prenotazione all'amministratore
+        await transporter.sendMail(adminMailOptions);
+        console.log("Notifica di prenotazione inviata con successo all'amministratore!");
     } catch (error) {
-        console.error(
-            "Errore durante l'invio della notifica via email:",
-            error
-        );
+        console.error("Errore durante l'invio della notifica via email:", error);
     }
 };
 
