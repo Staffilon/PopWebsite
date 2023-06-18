59c5375a6e10178d09570036fd75417b4706d6c4
Staffilon <stanislav.teghipco@studenti.unicam.it>
2023-06-18 10:07:18 +0200

    Atomic commit

--- backend/index.js
+++ backend/index.js
@@ -52,5 +52,5 @@ app.use(errorHandler);
 
 // Start the server
 app.listen(port, () => {
-    console.log("Server running on port " + port);
+    console.log("Server running on port: " + port);
 });
